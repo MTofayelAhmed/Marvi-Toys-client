d0a46af8c40be13993d063a0b3d8e75ca85dd6a8
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-05-19 23:16:55 +0600

    banner done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,8 +1,18 @@
 
 const Banner = () => {
   return (
-<div>
-<h1>this is banner</h1>
+<div className="mx-10">
+
+<div className="hero min-h-screen rounded-lg" style={{ backgroundImage: `url("https://i.ibb.co/8b1yKzn/cover-2.jpg")` }}>
+  <div className="hero-overlay bg-opacity-40"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md ">
+      <h1 className="mb-5 text-5xl font-bold">Welcome to Toy Land</h1>
+     
+      <button className="btn bg-red-950 ">Grab Your favorite hero </button>
+    </div>
+  </div>
+</div>
 
 </div>
   );
